540f4ced8550ebe52464ef6490549a6f8d74500e
sharora <sharora@gmail.com>
2018-04-21 14:11:32 -0700

    keyboard

---
+++ keyboard.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+def shoot():
+	pyautogui.press("m")
+def right():
+	pyautogui.press("right")
+def left():
+	pyautogui.press("left")
+def forward():
+	pyautogui.press("up")
+def backward():
+	pyautogui.press("down")
+
