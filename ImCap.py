0cedfb249ff18dbaf624f4e3adc740c5fed362f2
Ayush Dewan <ayushdewan02@gmail.com>
2018-04-21 13:48:21 -0700

    added image capture

---
+++ ImCap.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtGui
+import numpy as np
+import cv2
+from PIL import Image
+
+
+
+
+
+
+def takeimage():
+    global boom
+    app = QApplication(sys.argv)
+    screen = QGuiApplication.primaryScreen()
+    desktopPixmap = screen.grabWindow(0,464,170,180,275)
+    qimage1 = desktopPixmap.toImage()
+    bytes =qimage1.bits().asstring(qimage1.byteCount())
+    pilimg = Image.frombuffer("RGBA",(qimage1.width(),qimage1.height()),bytes,'raw', "RGBA", 0, 1)
+    img = np.array(pilimg)
+    boom = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return boom
+    
+
+boom = takeimage()
+
+ 
+
+
